1bdacfe36f4898a0b6a1043da9284b29dabdcfa1
Robert Henderson <robert@nterpryz.com>
2018-01-13 18:13:44 +0000

    created guest lass and tested guest has a name

--- src/main/java/Guest.java
+++ src/main/java/Guest.java
@@ -1,2 +1,17 @@
 public class Guest {
+
+    private int guestId;
+    private String name;
+    private int age;
+
+    public Guest(int guestId, String name, int age){
+        this.guestId = guestId;
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
